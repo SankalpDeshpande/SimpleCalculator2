2e2ebba9a1cbd96ca644e8e0a7997327d213f934
sanki18 <sankalpsmg@gmail.com>
2020-08-23 11:08:52 +0530

    material grid used

--- src/buttons/buttons.jsx
+++ src/buttons/buttons.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import PropTypes from "prop-types";
 import Button from '@material-ui/core/Button';
+import Grid from '@material-ui/core/Grid';
 
 export default class Buttons extends React.Component {
   static propTypes = {
@@ -16,74 +17,74 @@ export default class Buttons extends React.Component {
 
   render() {
     return (
-      <div class="container">
-        <div class="row form-group">
-          <div class="col-md-3">
-            <Button variant="contained" class="block" id="highlight" onClick={() => this.handleClick("AC")}>AC</Button>
-          </div>
-          <div class="col-md-3">
-            <Button variant="contained" class="block" onClick={() => this.handleClick("+/-")}>+/-</Button>
-          </div>
-          <div class="col-md-3">
-            <Button variant="contained" class="block" onClick={() => this.handleClick("%")}>%</Button>
-          </div>
-          <div class="col-md-3">
-            <Button variant="contained" class="block" onClick={() => this.handleClick("/")}>/</Button>
-          </div>
-        </div>
-        <div class="row form-group">
-          <div class="col-md-3">
-            <Button variant="contained" class="block" onClick={() => this.handleClick("7")}>7</Button>
-          </div>
-          <div class="col-md-3">
-            <Button variant="contained" class="block" onClick={() => this.handleClick("8")}>8</Button>
-          </div>
-          <div class="col-md-3">
-            <Button variant="contained" class="block" onClick={() => this.handleClick("9")}>9</Button>
-          </div>
-          <div class="col-md-3">
-            <Button variant="contained" class="block" onClick={() => this.handleClick("X")}>X</Button>
-          </div>
-        </div>
-        <div class="row form-group">
-          <div class="col-md-3">
-            <Button variant="contained" class="block" onClick={() => this.handleClick("4")}>4</Button>
-          </div>
-          <div class="col-md-3">
-            <Button variant="contained" class="block" onClick={() => this.handleClick("5")}>5</Button>
-          </div>
-          <div class="col-md-3">
-            <Button variant="contained" class="block" onClick={() => this.handleClick("6")}>6</Button>
-          </div>
-          <div class="col-md-3">
-            <Button variant="contained" class="block" onClick={() => this.handleClick("-")}>-</Button>
-          </div>
-        </div>
-        <div class="row form-group">
-          <div class="col-md-3">
-            <Button variant="contained" class="block" onClick={() => this.handleClick("1")}>1</Button>
-          </div>
-          <div class="col-md-3">
-            <Button variant="contained" class="block" onClick={() => this.handleClick("2")}>2</Button>
-          </div>
-          <div class="col-md-3">
-            <Button variant="contained" class="block" onClick={() => this.handleClick("3")}>3</Button>
-          </div>
-          <div class="col-md-3">
-            <Button variant="contained" class="block" onClick={() => this.handleClick("+")}>+</Button>
-          </div>
-        </div>
-        <div class="row form-group">
-          <div class="col-md-6">
-            <Button variant="contained" class="block" onClick={() => this.handleClick("0")}>0</Button>
-          </div>
-          <div class="col-md-3">
-            <Button variant="contained" class="block" onClick={() => this.handleClick(".")}>.</Button>
-          </div>
-          <div class="col-md-3">
-            <Button variant="contained" class="block" id="highlight" onClick={() => this.handleClick("=")}>=</Button>
-          </div>
-        </div>
+      <div>
+      <Grid container spacing={3}>
+        <Grid item xs={3}>
+          <Button variant="contained" class="block" id="highlight" onClick={() => this.handleClick("AC")}>AC</Button>
+        </Grid>
+        <Grid item xs={3}>
+          <Button variant="contained" class="block" onClick={() => this.handleClick("+/-")}>+/-</Button>
+        </Grid>
+        <Grid item xs={3}>
+          <Button variant="contained" class="block" onClick={() => this.handleClick("%")}>%</Button>
+        </Grid>
+        <Grid item xs={3}>
+          <Button variant="contained" class="block" onClick={() => this.handleClick("/")}>/</Button>
+        </Grid>
+      </Grid>
+      <Grid container spacing={3}>
+        <Grid item xs={3}>
+          <Button variant="contained" class="block" onClick={() => this.handleClick("7")}>7</Button>
+        </Grid>
+        <Grid item xs={3}>
+          <Button variant="contained" class="block" onClick={() => this.handleClick("8")}>8</Button>
+        </Grid>
+        <Grid item xs={3}>
+          <Button variant="contained" class="block" onClick={() => this.handleClick("9")}>9</Button>
+        </Grid>
+        <Grid item xs={3}>
+          <Button variant="contained" class="block" onClick={() => this.handleClick("X")}>X</Button>
+        </Grid>
+      </Grid>
+      <Grid container spacing={3}>
+        <Grid item xs={3}>
+          <Button variant="contained" class="block" onClick={() => this.handleClick("4")}>4</Button>
+        </Grid>
+        <Grid item xs={3}>
+          <Button variant="contained" class="block" onClick={() => this.handleClick("5")}>5</Button>
+        </Grid>
+        <Grid item xs={3}>
+          <Button variant="contained" class="block" onClick={() => this.handleClick("6")}>6</Button>
+        </Grid>
+        <Grid item xs={3}>
+          <Button variant="contained" class="block" onClick={() => this.handleClick("-")}>-</Button>
+        </Grid>
+      </Grid>
+      <Grid container spacing={3}>
+        <Grid item xs={3}>
+          <Button variant="contained" class="block" onClick={() => this.handleClick("1")}>1</Button>
+        </Grid>
+        <Grid item xs={3}>
+          <Button variant="contained" class="block" onClick={() => this.handleClick("2")}>2</Button>
+        </Grid>
+        <Grid item xs={3}>
+          <Button variant="contained" class="block" onClick={() => this.handleClick("3")}>3</Button>
+        </Grid>
+        <Grid item xs={3}>
+          <Button variant="contained" class="block" onClick={() => this.handleClick("+")}>+</Button>
+        </Grid>
+      </Grid>
+      <Grid container spacing={3}>
+        <Grid item xs={6}>
+          <Button variant="contained" class="block" onClick={() => this.handleClick("0")}>0</Button>
+        </Grid>
+        <Grid item xs={3}>
+          <Button variant="contained" class="block" onClick={() => this.handleClick(".")}>.</Button>
+        </Grid>
+        <Grid item xs={3}>
+          <Button variant="contained" class="block" id="highlight" onClick={() => this.handleClick("=")}>=</Button>
+        </Grid>
+      </Grid>
       </div>
     );
   }
