31d28307870c4c295259b923eebfc9b5d5b0d2d9
Sankalp B Deshpande <sankalpsmg@gmail.com>
2023-01-10 20:45:25 +0530

    setup gh-pages

---
+++ build/precache-manifest.be3ef61e39597a020af966094ddcd984.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3439821538762c2f58c8fec2219f37e1",
+    "url": "/react-gh-pages/index.html"
+  },
+  {
+    "revision": "6421d0d91c9b761c9097",
+    "url": "/react-gh-pages/static/css/2.bf023f09.chunk.css"
+  },
+  {
+    "revision": "be610f95339a1134ef03",
+    "url": "/react-gh-pages/static/css/main.80ebd8ef.chunk.css"
+  },
+  {
+    "revision": "6421d0d91c9b761c9097",
+    "url": "/react-gh-pages/static/js/2.1ae5ca88.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/react-gh-pages/static/js/2.1ae5ca88.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "be610f95339a1134ef03",
+    "url": "/react-gh-pages/static/js/main.fafb05b0.chunk.js"
+  },
+  {
+    "revision": "c19f96774fe079344363",
+    "url": "/react-gh-pages/static/js/runtime-main.d2ac757c.js"
+  },
+  {
+    "revision": "448c34a56d699c29117adc64c43affeb",
+    "url": "/react-gh-pages/static/media/glyphicons-halflings-regular.448c34a5.woff2"
+  },
+  {
+    "revision": "89889688147bd7575d6327160d64e760",
+    "url": "/react-gh-pages/static/media/glyphicons-halflings-regular.89889688.svg"
+  },
+  {
+    "revision": "e18bbf611f2a2e43afc071aa2f4e1512",
+    "url": "/react-gh-pages/static/media/glyphicons-halflings-regular.e18bbf61.ttf"
+  },
+  {
+    "revision": "f4769f9bdb7466be65088239c12046d1",
+    "url": "/react-gh-pages/static/media/glyphicons-halflings-regular.f4769f9b.eot"
+  },
+  {
+    "revision": "fa2772327f55d8198301fdb8bcfc8158",
+    "url": "/react-gh-pages/static/media/glyphicons-halflings-regular.fa277232.woff"
+  }
+]);
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.c0ce63b60a0b4bdf449cb80a730b5f56.js"
+  "/react-gh-pages/precache-manifest.be3ef61e39597a020af966094ddcd984.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-gh-pages/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
