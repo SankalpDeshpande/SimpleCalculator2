74a932fe61d2c840d68573cf5a39ab58db6b46b5
Sankalp B Deshpande <sankalpsmg@gmail.com>
2023-01-10 21:25:57 +0530

    Updates

---
+++ precache-manifest.79fa5526793135a0d6f64ac543250a10.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "25e2f866900ece3b213517faa36c192b",
+    "url": "/SimpleCalculator/index.html"
+  },
+  {
+    "revision": "8f53c40c146deef69b40",
+    "url": "/SimpleCalculator/static/css/2.e455b29e.chunk.css"
+  },
+  {
+    "revision": "71ceda4036f9bd43098b",
+    "url": "/SimpleCalculator/static/css/main.80ebd8ef.chunk.css"
+  },
+  {
+    "revision": "8f53c40c146deef69b40",
+    "url": "/SimpleCalculator/static/js/2.1ae5ca88.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/SimpleCalculator/static/js/2.1ae5ca88.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "71ceda4036f9bd43098b",
+    "url": "/SimpleCalculator/static/js/main.d58f0e93.chunk.js"
+  },
+  {
+    "revision": "15ddd7b36521ebd72f08",
+    "url": "/SimpleCalculator/static/js/runtime-main.efc9d2d0.js"
+  },
+  {
+    "revision": "448c34a56d699c29117adc64c43affeb",
+    "url": "/SimpleCalculator/static/media/glyphicons-halflings-regular.448c34a5.woff2"
+  },
+  {
+    "revision": "89889688147bd7575d6327160d64e760",
+    "url": "/SimpleCalculator/static/media/glyphicons-halflings-regular.89889688.svg"
+  },
+  {
+    "revision": "e18bbf611f2a2e43afc071aa2f4e1512",
+    "url": "/SimpleCalculator/static/media/glyphicons-halflings-regular.e18bbf61.ttf"
+  },
+  {
+    "revision": "f4769f9bdb7466be65088239c12046d1",
+    "url": "/SimpleCalculator/static/media/glyphicons-halflings-regular.f4769f9b.eot"
+  },
+  {
+    "revision": "fa2772327f55d8198301fdb8bcfc8158",
+    "url": "/SimpleCalculator/static/media/glyphicons-halflings-regular.fa277232.woff"
+  }
+]);
